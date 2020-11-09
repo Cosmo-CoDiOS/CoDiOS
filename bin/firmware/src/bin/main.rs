8acd78d1ae6d765f5c9d33fb1409e4307260740b
Dom Rodriguez <shymega@shymega.org.uk>
2020-11-09 19:21:53 +0000

    Major refactor
    - Use Workspace manifest
    - Split final firmware binary to `/bin/firmware`
    - Prepare for microkernel design
      - Graphics subsystem is a separate crate
      - Kernel crate has drivers, IPC, and RPC modules
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ bin/firmware/src/bin/main.rs
@@ -0,0 +1,29 @@
+//! The main firmware for the CoDi-rs RTOS
+#![no_std]
+#![no_main]
+#![deny(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::pedantic,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
+
+extern crate codirs_kernel as kernel;
+extern crate panic_semihosting as _;
+
+use cortex_m_rt::entry;
+use kernel::kernel_main;
+
+#[entry]
+fn main() -> ! {
+    kernel_main()
+}
