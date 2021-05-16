3c6f979fa74b409e70b69e90a097c217160fb50f
Dom Rodriguez <shymega@shymega.org.uk>
2021-05-16 18:14:17 +0100

    Add additional IPC result to enum
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- lib/codirs/kernel/src/ipc/mod.rs
+++ lib/codirs/kernel/src/ipc/mod.rs
@@ -20,10 +20,14 @@
 #[derive(Debug, Copy, Clone)]
 #[allow(non_camel_case_types, dead_code)]
 pub enum IPCResult {
-    /// This is a 'ACKNOWLEDGE' result, which is when a IPC call was received and processed by a process.
+    /// This is a `ACKNOWLEDGE` result, which is when a IPC call was received and processed by a
+    /// process.
     RESULT_ACK,
     /// This is a `REJECT` result, which is when a IPC call to a process was rejected.
     RESULT_REJ,
-    /// This is a 'ILLEGAL' result, which is when a IPC call to a process was determined to be unsupported/illegal.
+    /// This is a `ILLEGAL` result, which is when a IPC call to a process was determined to be
+    /// unsupported/illegal.
     RESULT_IGL,
+    /// This is a `NOP` result, which is when a IPC call to a process does not return any result.
+    RESULT_NOP,
 }
