d367163fd9aa8e143bb38b45d751e83bcb73e15f
Dom Rodriguez <shymega@shymega.org.uk>
2024-04-13 20:40:41 +0100

    refactor: Fix builds with Embassy
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/bin/codios-firmware.rs
+++ src/bin/codios-firmware.rs
@@ -1,5 +1,6 @@
 //! This is the main firmware image for the `CoDiOS` firmware.
 #![cfg_attr(all(target_arch = "arm", feature = "firmware", not(feature = "emulator")), no_main, no_std)]
+#![feature(impl_trait_in_assoc_type)]
 #![deny(
     warnings,
     missing_copy_implementations,
@@ -16,6 +17,14 @@
     unused_extern_crates,
     variant_size_differences
 )]
+
+#[cfg(all(feature = "firmware", target_arch = "arm"))]
+use defmt::*;
+#[cfg(all(feature = "firmware", target_arch = "arm"))]
+use {defmt_rtt as _, panic_probe as _};
+
+use embassy_executor::Spawner;
+use codios::kmain;
 
 #[cfg(all(target_arch = "arm", feature = "firmware", not(feature = "emulator")))]
 fn main() -> ! {
