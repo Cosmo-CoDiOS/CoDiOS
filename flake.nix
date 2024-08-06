2cad8b9c8e1d6b8a85a39f5ca406b13936c2cc6a
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-06 20:13:00 +0100

    chore: Bump all deps, lockfiles, Flake, and rustc
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -1,54 +1,80 @@
-# SPDX-FileCopyrightText: 2023 The Booters Developers
+# SPDX-FileCopyrightText: 2024 The Cosmo-CoDiOS Developers
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
-
-    nixpkgs-mozilla = {
-      url = "github:mozilla/nixpkgs-mozilla";
-      flake = false;
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, flake-utils, nixpkgs, nixpkgs-mozilla }:
+  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
           inherit system;
-
-          overlays = [
-            (import nixpkgs-mozilla)
-          ];
         };
 
-        toolchain = (pkgs.rustChannelOf {
-          date = "2024-04-11";
-          channel = "nightly";
-          sha256 = "sha256-dPMfc+32T+p/DluUaN6qJk1+qAYbsYYMbZMmwzaldzs=";
-        }).rust.override {
-          targets = [
-            "x86_64-unknown-linux-musl"
-            "thumbv7em-none-eabihf"
-            "aarch64-unknown-linux-musl"
-          ];
-          extensions = [
-            "rust-src"
-            "rustfmt-preview"
-            "clippy-preview"
-            "llvm-tools-preview"
-            "rust-std"
-          ];
+        toolchain = with fenix.packages.${system}; fromToolchainFile {
+          dir = ./.;
+          sha256 = "sha256-oW7iyYzGcgW5TjRA2HLhYVW2WNTNadIe4SX7IWsrs3g=";
         };
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        naerskBuildPackage = target: args:
+          naersk'.buildPackage (
+            args
+            // { CARGO_BUILD_TARGET = target; }
+          );
       in
+      rec
       {
-        # For `nix develop` (optional, can be skipped):
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = [ toolchain ] ++ (with pkgs; [ pkg-config ]);
-          buildInputs = with pkgs; [ systemd.dev protobuf SDL SDL.dev SDL2 SDL2.dev ];
+        defaultPackage = packages.x86_64-unknown-linux-musl;
+
+        # For `nix build .#x86_64-unknown-linux-musl`:
+        packages.x86_64-unknown-linux-musl = naerskBuildPackage "x86_64-unknown-linux-musl" rec {
+          src = ./.;
+          doCheck = true;
+          nativeBuildInputs = with pkgs; [ pkgsStatic.stdenv.cc cmake SDL SDL2 SDL.dev SDL2.dev ];
+          buildInputs = nativeBuildInputs;
         };
-      }
-    );
+
+        # For `nix build .#x86_64-unknown-linux-musl`:
+        packages.aarch64-unknown-linux-musl = naerskBuildPackage "aarch64-unknown-linux-musl" rec {
+          src = ./.;
+          doCheck = true;
+          nativeBuildInputs = with pkgs; [ pkgsStatic.stdenv.cc cmake SDL SDL2 SDL.dev SDL2.dev ];
+          buildInputs = nativeBuildInputs;
+        };
+
+        # For `nix build .#x86_64-unknown-linux-musl`:
+        packages.thumbv7em-none-eabihf = naerskBuildPackage "thumbv7em-none-eabihf" {
+          src = ./.;
+          doCheck = true;
+          nativeBuildInputs = with pkgs; [ cmake ];
+        };
+
+        devShell = pkgs.mkShellNoCC (
+          {
+            inputsFrom = [ packages.x86_64-unknown-linux-musl packages.aarch64-unknown-linux-musl ];
+            packages = with pkgs; [
+              rustup
+              cargo-cross
+            ];
+            CROSS_CONTAINER_OPTS = "--platform linux/amd64";
+            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          });
+      });
 }
